3490726c52bc00df5edc18fc9a2e60bd7a1c1ba6
Smita <smita23tiwari.007@gmail.com>
2024-08-30 16:36:33 +0530

    lecture1.php

---
+++ lecture1.php
@@ -0,0 +1,31 @@
+<?php
+// echo "<h1>Hello World</   h1>";
+// echo " Welcome "," ", " to LPU ";  // for concationation "." or "," is used   
+// $ var_name= 20;
+
+// Task 1
+// $greeting = "Hello";
+// $name= "Smita";
+// echo $greeting, " ",$name;
+
+//  Task 2
+// $num=10;
+// $res= 10*2-5+3;
+// echo $res;
+
+//  Task 3
+// $pi=3.14159;
+// $rad=2;
+// $circum=2*$pi*$rad;
+// echo $circum;
+
+// Task 4:
+$a=5;
+$b=10;
+$a=$a+$b;  
+$b=$a-$b;  
+$a=$a-$b;    
+echo "a after swapping is ",$a ," and b after swapping is ",$b;
+
+
+?>

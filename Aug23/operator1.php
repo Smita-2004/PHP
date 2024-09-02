205894208181ca25c85e4335c85ba752503464a0
Smita Tiwari <114800220+Smita-2004@users.noreply.github.com>
2024-09-02 15:57:37 +0530

    Add files via upload

---
+++ Aug23/operator1.php
@@ -0,0 +1,9 @@
+<?php
+$a=readline("Enter value for a : ");
+$b=readline("Enter value for b : ");
+$c=readline("Enter value for c : ");  
+$d=readline("Enter value for d : ");  
+$e=readline("Enter value for e : ");  
+// $res=($a+$b)*$c/$d-$e;
+echo "Result for expression (a+b)*/d-e is: ",($a+$b)*$c/$d-$e;
+?>

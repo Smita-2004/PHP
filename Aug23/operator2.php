b98d808ca21450a775940cfda061873b15c4dbb0
Smita Tiwari <114800220+Smita-2004@users.noreply.github.com>
2024-09-06 09:15:34 +0530

    Add files via upload

---
+++ Aug23/operator2.php
@@ -0,0 +1,13 @@
+<?php
+$num=readline("Enter a number: ");
+$a=readline("Enter value for a : ");
+$b=readline("Enter value for b : ");
+$c=readline("Enter value for c : ");  
+$d=readline("Enter value for d : ");  
+$e=readline("Enter value for e : ");  
+echo $a+="a+= ", $num, "\n";
+echo $b-= "b-=" ,$num, "\n";
+echo $c*= "c*= ", $num, "\n";
+echo $d/= "d/= ",$num, "\n";
+echo $e%="e%= ", $num, "\n";
+?>

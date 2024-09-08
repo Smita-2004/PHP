e2f6d4b29c9880fd86df0d3c6fb764a4e921d67c
Smita Tiwari <114800220+Smita-2004@users.noreply.github.com>
2024-09-08 09:58:49 +0530

    Add files via upload

---
+++ Aug28/question4.php
@@ -0,0 +1,31 @@
+<!-- <?php
+$a=readline("Choose any of these
+1. Breakfast
+2. Lunch
+3. Dinner
+4. Snack\n");
+switch($a){
+    case 1:
+        $b=readline("Select any of the meal\n");
+        $z= "Sandwitch";
+       $g= " Corn flakes";
+        $h= "Parantha";
+    case 2:
+        $b=readline("Select any of the meal\n");
+        $q= "Rajma chawal";
+        $w= "Chole bhture";
+        $r= "Brown rice";
+    case 3:
+        $b=readline("Select any of the meal\n");
+        $t= "Salad";
+        $y= "Chapatti";
+        $u= "Raita";
+    case 4:
+        $b=readline("Select any of the meal\n");
+        $i="Tea";
+        $o=" Coffee";
+        $p=" Pakoda";
+}
+echo "Your order  ",$b," will be ready in few minutes.Happy meal!";
+
+?> -->

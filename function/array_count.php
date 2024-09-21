e1e5e0742975096102755c83a7ab08f4775749d4
Smita Tiwari <114800220+Smita-2004@users.noreply.github.com>
2024-09-21 08:41:59 +0530

    Add files via upload

---
+++ function/array_count.php
@@ -0,0 +1,12 @@
+<?php
+// $a=array("Block 33","Block 34","Block 34","Block 35","Block 36");
+// print_r(array_count_values($a));
+
+$food=array(
+    $fruits=array("Orange","Apple","Pineapple"),
+    $veg=array("Chilli","Potato","Tomato"),
+
+);
+print_r(array_count_values($fruits));  
+
+?>

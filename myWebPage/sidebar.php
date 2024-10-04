b944196dfb86d5cbec39be5c9a39b2e3e69d4142
Smita Tiwari <114800220+Smita-2004@users.noreply.github.com>
2024-10-04 11:06:17 +0530

    Add files via upload

---
+++ myWebPage/sidebar.php
@@ -0,0 +1,8 @@
+<aside class="sidebar">
+    
+    <ul>
+        <li><a href="#">Conditions</a></li>
+        <li><a href="#">Our Policy</a></li>
+        <li><a href="#">Contact us</a></li>
+    </ul>
+</aside>

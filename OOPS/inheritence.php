b93dc91b5403ee7e05379a87c116dc3b9898a1f0
Smita Tiwari <114800220+Smita-2004@users.noreply.github.com>
2024-11-15 23:25:52 +0530

    Add files via upload

---
+++ OOPS/inheritence.php
@@ -0,0 +1,11 @@
+<?php
+class employee{
+    public $name, $age, $salary;
+    public function __construct($n, $a, $sal){
+        $this->name =$n ;
+        $this-> age = $a;
+        $this-> salary= $sal ;
+
+        }
+}
+?>          
